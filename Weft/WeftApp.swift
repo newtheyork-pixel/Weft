e99f03338e52f0bd2a0a1f89ed46a59616f70bfa
thomas seirer <newtheyork@gmail.com>
2026-06-08 23:10:44 -0400

    Initial Commit

---
+++ Weft/WeftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeftApp.swift
+//  Weft
+//
+//  Created by Thomas Seirer on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct WeftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
